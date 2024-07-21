73c871f52aef2b63add740f3ff5df88efa4a035e
l3r8yJ <l3r8y@duck.com>
2024-07-21 14:26:38 +0300

    feat(#1): add queue

---
+++ src/queue.zig
@@ -0,0 +1,80 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2024 Ivan Ivanchuk
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+const std = @import("std");
+
+pub fn Queue(comptime Child: type) type {
+    return struct {
+        const This = @This();
+        const Node = struct {
+            data: Child,
+            next: ?*Node,
+        };
+        gpa: std.mem.Allocator,
+        start: ?*Node,
+        end: ?*Node,
+
+        pub fn init(gpa: std.mem.Allocator) This {
+            return This{
+                .gpa = gpa,
+                .start = null,
+                .end = null,
+            };
+        }
+        pub fn enqueue(this: *This, value: Child) !void {
+            const node = try this.gpa.create(Node);
+            node.* = .{ .data = value, .next = null };
+            if (this.end) |end| end.next = node else this.start = node;
+            this.end = node;
+        }
+        pub fn dequeue(this: *This) ?Child {
+            const start = this.start orelse return null;
+            defer this.gpa.destroy(start);
+            if (start.next) |next|
+                this.start = next
+            else {
+                this.start = null;
+                this.end = null;
+            }
+            return start.data;
+        }
+    };
+}
+
+test "queue" {
+    var int_queue = Queue(i32).init(std.testing.allocator);
+
+    try int_queue.enqueue(25);
+    try int_queue.enqueue(50);
+    try int_queue.enqueue(75);
+    try int_queue.enqueue(100);
+
+    try std.testing.expectEqual(int_queue.dequeue(), 25);
+    try std.testing.expectEqual(int_queue.dequeue(), 50);
+    try std.testing.expectEqual(int_queue.dequeue(), 75);
+    try std.testing.expectEqual(int_queue.dequeue(), 100);
+    try std.testing.expectEqual(int_queue.dequeue(), null);
+
+    try int_queue.enqueue(5);
+    try std.testing.expectEqual(int_queue.dequeue(), 5);
+    try std.testing.expectEqual(int_queue.dequeue(), null);
+}
